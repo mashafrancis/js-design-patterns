a6f27c7f06dd180c60d9682f1651dd85401d23f3
Francis Masha <francismasha96@gmail.com>
2021-06-03 10:38:20 +0300

    mvp pattern

---
+++ architectural/mvp/challenge(email).js
@@ -0,0 +1,90 @@
+class Model {
+  constructor() {
+    this.senderName = "";
+    this.receiverName = "";
+    this.emailTitle = "";
+  }
+
+  setSenderName(senderName) {
+    this.senderName = senderName;
+  }
+
+  getSenderName() {
+    return this.senderName;
+  }
+
+  setReceiverName(receiverName) {
+    this.receiverName = receiverName;
+  }
+
+  getReceiverName() {
+    return this.receiverName;
+  }
+
+  setEmailTitle(emailTitle) {
+    this.emailTitle = emailTitle;
+  }
+
+  getEmailTitle() {
+    return this.emailTitle;
+  }
+}
+
+class View {
+  constructor() {
+    this.presenter = null;
+  }
+
+  registerWith(presenter) {
+    this.presenter = presenter;
+  }
+
+  sendEmail(to, fromWhom, emailTitle) {
+    this.presenter.sendEmail(to, fromWhom, emailTitle);
+  }
+
+  displayEmailInfo(senderName, receiverName, emailTitle) {
+    console.log(
+      "Email From: " +
+        senderName +
+        " To: " +
+        receiverName +
+        " Title: " +
+        emailTitle
+    );
+  }
+}
+
+class Presenter {
+  constructor(view) {
+    this.view = view;
+    this.model = null;
+  }
+
+  setModel(model) {
+    this.model = model;
+  }
+
+  getView() {
+    return this.view;
+  }
+
+  sendEmail(to, fromWhom, emailTitle) {
+    this.model.setEmailTitle(emailTitle);
+    this.model.setSenderName(fromWhom);
+    this.model.setReceiverName(to);
+    this.view.displayEmailInfo(
+      this.model.getSenderName(),
+      this.model.getReceiverName(),
+      this.model.getEmailTitle()
+    );
+  }
+}
+
+const model = new Model();
+const view = new View();
+const presenter = new Presenter(view);
+presenter.setModel(model);
+view.registerWith(presenter);
+presenter.getView().sendEmail("Rachel", "Joey", "Rent Discussion");
+presenter.getView().sendEmail("Monica", "Phoebe", "Smelly Cat Draft");
